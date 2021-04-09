b69bac9c9ab424f24049338bda9530370e066da2
shik <gengshikun@itiger.com>
2021-04-09 17:47:31 +0800

    【add】迁移元数据管理接口到本项目

---
+++ persona-platform-web/src/main/java/com/itiger/persona/service/mapper/SignatureMapper.java
@@ -0,0 +1,18 @@
+package com.itiger.persona.service.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.itiger.persona.entity.Signature;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author shik
+ * @since 2020-10-16
+ */
+@Mapper
+public interface SignatureMapper extends BaseMapper<Signature> {
+
+}
