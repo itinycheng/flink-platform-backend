d029c233844bfd8fff62869bee95085bcb2bda46
chenglong.wang <tiny.wcl@gmail.com>
2021-04-26 12:08:30 +0800

    sql submit test and bug fix

--- persona-platform-web/src/test/java/com/itiger/persona/web/FileTest.java
+++ persona-platform-web/src/test/java/com/itiger/persona/web/FileTest.java
@@ -1,9 +1,13 @@
 package com.itiger.persona.web;
 
-import org.apache.hadoop.fs.Path;
+import org.apache.commons.io.FileUtils;
 import org.junit.Test;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class FileTest {
 
@@ -14,8 +18,9 @@ public class FileTest {
     }
 
     @Test
-    public void test2() {
-        Path path = new Path("/Users/tiger/IdeaProjects/persona-platform-backend/persona-platform-web/application.yml");
-        System.out.println(path.toUri().toString());
+    public void test2() throws IOException {
+        Path path = Paths.get("tmp/t.log");
+        FileUtils.write(path.toFile(), "data", StandardCharsets.UTF_8);
+        System.out.println("~~~" + path.toAbsolutePath().toString());
     }
 }
