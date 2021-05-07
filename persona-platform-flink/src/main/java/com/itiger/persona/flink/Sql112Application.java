96bdab0ed71cc4bfcc14ca8578c54058b4f6e8ba
chenglong.wang <tiny.wcl@gmail.com>
2021-05-07 20:28:36 +0800

    set configs once the tEnv is created

--- persona-platform-flink/src/main/java/com/itiger/persona/flink/Sql112Application.java
+++ persona-platform-flink/src/main/java/com/itiger/persona/flink/Sql112Application.java
@@ -2,7 +2,9 @@ package com.itiger.persona.flink;
 
 import com.itiger.persona.common.entity.job.Catalog;
 import com.itiger.persona.common.entity.job.Function;
+import com.itiger.persona.common.entity.job.Sql;
 import com.itiger.persona.common.entity.job.SqlContext;
+import com.itiger.persona.common.enums.SqlType;
 import com.itiger.persona.common.util.JsonUtil;
 import com.itiger.persona.flink.common.ConfigLoader;
 import com.itiger.persona.flink.helper.Catalogs;
@@ -16,6 +18,9 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.Map;
+
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toMap;
 
 /**
  * must input a valid sql context file
@@ -33,6 +38,10 @@ public class Sql112Application {
         TableEnvironment tEnv = ExecutionEnvs.createExecutionEnv(sqlContext.getExecMode());
         Map<String, String> configMap = ConfigLoader.loadDefault(sqlContext.getExecMode());
         configMap.putAll(sqlContext.getConfigs());
+        configMap.putAll(sqlContext.getSqls().stream()
+                .filter(sql -> SqlType.SET.equals(sql.getType()))
+                .map(Sql::getOperands)
+                .collect(toMap(operands -> operands[0], operands -> operands[1])));
         configMap.forEach((key, value) -> Configurations.setConfig(tEnv, key, value));
 
         // step 2: add catalog
@@ -44,7 +53,10 @@ public class Sql112Application {
         Functions.registerFunctionsToTableEnv(tEnv, functions);
 
         // step 4: exec sql
-        ExecuteSqls.execSqls(tEnv, sqlContext.getSqls());
+        List<Sql> executableSqls = sqlContext.getSqls().stream()
+                .filter(sql -> !SqlType.SET.equals(sql.getType()))
+                .collect(toList());
+        ExecuteSqls.execSqls(tEnv, executableSqls);
     }
 
 }
