da4a5253c2b9b40a58443c108ad358c50eda8fb6
chenglong.wang <tiny.wcl@gmail.com>
2021-05-14 11:26:38 +0800

    add monitor module

---
+++ persona-platform-monitor/src/main/java/com/itiger/persona/monitor/App.java
@@ -0,0 +1,13 @@
+package com.itiger.persona.monitor;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
