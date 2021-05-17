03bcdc4660d275043143caceb541fe9979ec8737
chenglong.wang <tiny.wcl@gmail.com>
2021-05-17 19:31:41 +0800

    json2sql, add column family

--- persona-platform-web/src/main/java/com/itiger/persona/constants/UserGroupConst.java
+++ persona-platform-web/src/main/java/com/itiger/persona/constants/UserGroupConst.java
@@ -9,5 +9,12 @@ public class UserGroupConst {
 
     public static final SqlIdentifier SOURCE_TABLE_IDENTIFIER = new SqlIdentifier("v1", "t_hive_user_signature_data_v1");
 
+    public static final String UUID = "uuid";
+
+    public static final String BUS = "bus";
+
+    public static final String IB = "ib";
+
+    public static final String COMMON = "common";
 
 }
