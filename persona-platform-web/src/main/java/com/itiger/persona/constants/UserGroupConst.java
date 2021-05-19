424d1768a37f1896397d8781fe36b0c0f7e65691
chenglong.wang <tiny.wcl@gmail.com>
2021-05-19 12:11:11 +0800

    json2sql, add hive jdbc

--- persona-platform-web/src/main/java/com/itiger/persona/constants/UserGroupConst.java
+++ persona-platform-web/src/main/java/com/itiger/persona/constants/UserGroupConst.java
@@ -9,6 +9,14 @@ public class UserGroupConst {
 
     public static final SqlIdentifier SOURCE_TABLE_IDENTIFIER = new SqlIdentifier("v1", "t_hive_user_signature_data_v1");
 
+    public static final String QUERY_SOURCE_TABLE_PARTITIONS = "show partitions t_hive_user_signature_data_v1 PARTITION(account_type = 'BUS')";
+
+    public static final String SOURCE_TABLE_DT_PARTITION = "dt";
+
+    public static final String SOURCE_TABLE_ACCOUNT_TYPE_PARTITION = "account_type";
+
+    public static final String DT_PARTITION_PREFIX = "dt=";
+
     public static final String UUID = "uuid";
 
     public static final String BUS = "bus";
---
+++ persona-platform-web/src/main/java/com/itiger/persona/controller/QuartzController.java
@@ -0,0 +1,10 @@
+package com.itiger.persona.controller;
+
+/**
+ * all about quartz operations
+ *
+ * @author tiny.wang
+ */
+public class QuartzController {
+
+}
