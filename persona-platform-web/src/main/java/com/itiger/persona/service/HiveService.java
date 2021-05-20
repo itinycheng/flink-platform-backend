0f0bd14f2177c0167b11df22a27c7b633f0d9eea
chenglong.wang <tiny.wcl@gmail.com>
2021-05-20 14:05:57 +0800

    json2sql, add Map sql type support and cache hive partitions

--- persona-platform-web/src/main/java/com/itiger/persona/service/HiveService.java
+++ persona-platform-web/src/main/java/com/itiger/persona/service/HiveService.java
@@ -2,6 +2,7 @@ package com.itiger.persona.service;
 
 import com.baomidou.dynamic.datasource.annotation.DS;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
@@ -21,6 +22,7 @@ public class HiveService {
     @Resource
     private JdbcTemplate jdbcTemplate;
 
+    @Cacheable("caffeine")
     public List<String> getList(String sql) {
         return jdbcTemplate.queryForList(sql, String.class);
     }
