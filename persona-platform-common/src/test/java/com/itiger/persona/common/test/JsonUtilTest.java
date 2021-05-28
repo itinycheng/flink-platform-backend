6800ce861a3cd8123db777f7fc34d14b6ae83555
chenglong.wang <tiny.wcl@gmail.com>
2021-05-28 15:17:24 +0800

    add position UDTF

--- persona-platform-common/src/test/java/com/itiger/persona/common/test/JsonUtilTest.java
+++ persona-platform-common/src/test/java/com/itiger/persona/common/test/JsonUtilTest.java
@@ -10,6 +10,7 @@ import org.junit.Test;
 
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class JsonUtilTest {
@@ -43,5 +44,12 @@ public class JsonUtilTest {
         catalog.setConfigs(map);
         String str = JsonUtil.toJsonString(catalog);
         System.out.println(str);
+    }
+
+    @Test
+    public void test0() {
+        String listString = "[{\"symbol\":\"BILI\",\"costLevel\":1,\"strike\":\"\",\"currency\":\"USD\",\"expiry\":\"\",\"right\":\"\",\"status\":\"LONG\",\"timestamp\":1621990862104},{\"symbol\":\"BIDU\",\"costLevel\":1,\"strike\":\"\",\"currency\":\"USD\",\"expiry\":\"\",\"right\":\"\",\"status\":\"LONG\",\"timestamp\":1621990862101}]";
+        List<String> strings = JsonUtil.toList(listString);
+        System.out.println(strings);
     }
 }
