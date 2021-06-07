b51737b3ae479e26555b99cd3a78e6f670a1b6e0
chenglong.wang <tiny.wcl@gmail.com>
2021-06-07 19:57:52 +0800

    quartz junit test

---
+++ persona-platform-web/src/test/java/com/itiger/persona/web/QuartzTest.java
@@ -0,0 +1,21 @@
+package com.itiger.persona.web;
+
+import org.junit.Test;
+import org.quartz.CronExpression;
+
+import java.text.ParseException;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.Date;
+
+public class QuartzTest {
+
+    @Test
+    public void test() throws ParseException {
+        CronExpression cronExpression = new CronExpression("0 43 1 * * ?");
+        LocalDateTime of = LocalDateTime.of(2021, 5, 21, 3, 0);
+        Date from = Date.from(of.toInstant(ZoneOffset.of("+8")));
+        System.out.println(cronExpression.getNextValidTimeAfter(from));
+        System.out.println(cronExpression.getNextValidTimeAfter(cronExpression.getNextValidTimeAfter(from)));
+    }
+}
