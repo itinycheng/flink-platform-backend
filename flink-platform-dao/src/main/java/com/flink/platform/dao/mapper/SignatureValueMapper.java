599304327b5699213c718c588aac5833aea57001
chenglong.wang <tiny.wcl@gmail.com>
2022-02-28 15:10:54 +0800

    Remove unused features

--- flink-platform-dao/src/main/java/com/flink/platform/dao/mapper/SignatureValueMapper.java
+++
@@ -1,7 +0,0 @@
-package com.flink.platform.dao.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.flink.platform.dao.entity.SignatureValue;
-
-/** Signature value mapper. */
-public interface SignatureValueMapper extends BaseMapper<SignatureValue> {}
