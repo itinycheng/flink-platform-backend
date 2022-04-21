f3985bd7f875d50b0e339b196507a140dd961783
chenglong.wang <tiny.wcl@gmail.com>
2022-04-21 10:42:45 +0800

    [bug fix] Run job alone and wait for change status to complete

--- flink-platform-web/src/main/java/com/flink/platform/web/quartz/JobRunner.java
+++ flink-platform-web/src/main/java/com/flink/platform/web/quartz/JobRunner.java
@@ -1,37 +1,32 @@
 package com.flink.platform.web.quartz;
 
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.flink.platform.common.enums.JobStatus;
-import com.flink.platform.dao.entity.JobInfo;
-import com.flink.platform.dao.entity.JobRunInfo;
-import com.flink.platform.dao.service.JobInfoService;
+import com.flink.platform.common.model.JobVertex;
 import com.flink.platform.web.common.SpringContext;
-import com.flink.platform.web.service.ProcessJobService;
-import com.flink.platform.web.service.WorkerApplyService;
+import com.flink.platform.web.config.WorkerConfig;
+import com.flink.platform.web.runner.JobExecuteThread;
+import com.flink.platform.web.runner.JobResponse;
+import com.flink.platform.web.util.ThreadUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.quartz.Job;
 import org.quartz.JobDetail;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobKey;
-import org.springframework.web.client.RestTemplate;
 
-import static com.flink.platform.web.util.HttpUtil.isRemoteUrl;
+import java.util.concurrent.ThreadPoolExecutor;
 
-/** submit job. */
+import static com.flink.platform.common.enums.ExecutionCondition.AND;
+
+/**
+ * submit single job. <br>
+ * TODO: handle the case that thread pool fully used.
+ */
 @Slf4j
 public class JobRunner implements Job {
 
-    private static final String REST_JOB_PROCESS = "/internal/process/%s";
+    public static final ThreadPoolExecutor EXECUTOR =
+            ThreadUtil.newFixedThreadExecutor("JobRunnerExecutor", 100);
 
-    private final JobInfoService jobInfoService = SpringContext.getBean(JobInfoService.class);
-
-    private final WorkerApplyService workerApplyService =
-            SpringContext.getBean(WorkerApplyService.class);
-
-    private final RestTemplate restTemplate = SpringContext.getBean(RestTemplate.class);
-
-    private final ProcessJobService processJobService =
-            SpringContext.getBean(ProcessJobService.class);
+    private final WorkerConfig workerConfig = SpringContext.getBean(WorkerConfig.class);
 
     @Override
     public void execute(JobExecutionContext context) {
@@ -39,35 +34,13 @@ public class JobRunner implements Job {
         JobKey key = detail.getKey();
         Long jobId = Long.parseLong(key.getName());
 
-        try {
-            // Step 1: get job info
-            JobInfo jobInfo =
-                    jobInfoService.getOne(
-                            new QueryWrapper<JobInfo>()
-                                    .lambda()
-                                    .eq(JobInfo::getId, jobId)
-                                    .eq(JobInfo::getStatus, JobStatus.ONLINE));
-
-            if (jobInfo == null) {
-                log.warn("The job:{} is no longer exists or not in ready/scheduled status.", jobId);
-                return;
-            }
-
-            // Step 2: build cluster url, set localhost as default url if not specified.
-            String routeUrl = workerApplyService.chooseWorker(jobInfo.getRouteUrl());
-
-            // Step 3: process job.
-            JobRunInfo jobRunInfo;
-            if (isRemoteUrl(routeUrl)) {
-                String httpUri = routeUrl + String.format(REST_JOB_PROCESS, jobId);
-                jobRunInfo = restTemplate.getForObject(httpUri, JobRunInfo.class);
-            } else {
-                jobRunInfo = processJobService.processJob(jobId, null);
-            }
-
-            log.info("The job: {} is processed, job run id: {}", jobId, jobRunInfo.getId());
-        } catch (Exception e) {
-            log.error("Cannot exec job: {}", jobId, e);
-        }
+        JobVertex jobVertex = new JobVertex(jobId, jobId);
+        jobVertex.setPrecondition(AND);
+        EXECUTOR.execute(
+                () -> {
+                    JobExecuteThread callable = new JobExecuteThread(null, jobVertex, workerConfig);
+                    JobResponse response = callable.call();
+                    log.info("The job: {} is processed, result: {}", jobId, response);
+                });
     }
 }
