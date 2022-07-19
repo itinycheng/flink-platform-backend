9a2ea161c3cb074b597786bdd9367a561d474fbb
chenglong.wang <tiny.wcl@gmail.com>
2022-07-19 20:36:00 +0800

    Clean up useless code

--- flink-platform-common/src/main/java/com/flink/platform/common/constants/Constant.java
+++ flink-platform-common/src/main/java/com/flink/platform/common/constants/Constant.java
@@ -23,8 +23,6 @@ public class Constant {
 
     public static final String DOT = ".";
 
-    public static final String STAR = "*";
-
     public static final String SEMICOLON = ";";
 
     public static final String SPACE = " ";
@@ -35,19 +33,7 @@ public class Constant {
 
     public static final String SINGLE_QUOTE = "'";
 
-    public static final String DOUBLE_QUOTE = "\"";
-
-    public static final String BACK_TICK = "`";
-
-    public static final String UNDERSCORE = "_";
-
-    public static final String BRACKET_LEFT = "(";
-
-    public static final String BRACKET_RIGHT = ")";
-
     public static final String LINE_SEPARATOR = "\n";
-
-    public static final String EQUAL = "=";
 
     public static final String SLASH = "/";
 
