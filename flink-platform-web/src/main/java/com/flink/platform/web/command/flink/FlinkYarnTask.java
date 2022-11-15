3f5bef3a306cc56dd3b0677bff8f418f6e830ff5
chenglong.wang <tiny.wcl@gmail.com>
2022-11-15 12:50:18 +0800

    [bug fix] Record shell callback info of flink job and unified shell exec code

--- flink-platform-web/src/main/java/com/flink/platform/web/command/flink/FlinkYarnTask.java
+++ flink-platform-web/src/main/java/com/flink/platform/web/command/flink/FlinkYarnTask.java
@@ -4,13 +4,11 @@ import com.flink.platform.common.enums.DeployMode;
 import com.flink.platform.web.command.shell.ShellTask;
 import com.flink.platform.web.common.SpringContext;
 import com.flink.platform.web.external.YarnClientService;
-import com.flink.platform.web.util.CollectLogThread;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 
-import java.io.InputStream;
 import java.util.function.BiConsumer;
 import java.util.regex.Matcher;
 
@@ -18,10 +16,7 @@ import static com.flink.platform.common.constants.JobConstant.APP_ID_PATTERN;
 import static com.flink.platform.common.constants.JobConstant.JOB_ID_PATTERN;
 import static com.flink.platform.common.enums.DeployMode.FLINK_YARN_PER;
 import static com.flink.platform.web.util.CollectLogThread.CmdOutType;
-import static com.flink.platform.web.util.CollectLogThread.CmdOutType.ERR;
 import static com.flink.platform.web.util.CollectLogThread.CmdOutType.STD;
-import static com.flink.platform.web.util.CommandUtil.EXIT_CODE_FAILURE;
-import static java.util.concurrent.TimeUnit.MILLISECONDS;
 
 /** Flink yarn task. */
 @Slf4j
@@ -52,39 +47,7 @@ public class FlinkYarnTask extends ShellTask {
     }
 
     public void run() throws Exception {
-        log.info("Exec command: {}, env properties: {}", command, envs);
-        this.process = Runtime.getRuntime().exec(command, envs);
-        try (InputStream stdStream = process.getInputStream();
-                InputStream errStream = process.getErrorStream()) {
-            CollectLogThread stdThread = new CollectLogThread(stdStream, STD, logConsumer);
-            CollectLogThread errThread = new CollectLogThread(errStream, ERR, logConsumer);
-
-            try {
-                stdThread.start();
-                errThread.start();
-            } catch (Exception e) {
-                log.error("Start log collection thread failed", e);
-            }
-
-            this.exited = process.waitFor(timeoutMills, MILLISECONDS);
-            this.exitValue = exited ? process.exitValue() : EXIT_CODE_FAILURE;
-
-            try {
-                stdThread.interrupt();
-                stdThread.join();
-            } catch (Exception e) {
-                log.error("interrupt std log collection thread failed", e);
-            }
-
-            try {
-                errThread.interrupt();
-                errThread.join();
-            } catch (Exception e) {
-                log.error("interrupt err log collection thread failed", e);
-            }
-        } finally {
-            process.destroy();
-        }
+        super.run();
     }
 
     @Override
