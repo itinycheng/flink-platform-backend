de775ba7e3b998b614d9e7027706896795ec6fd4
chenglong.wang <tiny.wcl@gmail.com>
2022-12-06 11:30:48 +0800

    [bug fix] Get the first non-loopback ipv4 address as default host ip

--- flink-platform-common/src/main/java/com/flink/platform/common/util/OSUtil.java
+++ flink-platform-common/src/main/java/com/flink/platform/common/util/OSUtil.java
@@ -2,7 +2,13 @@ package com.flink.platform.common.util;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.net.Inet4Address;
 import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
 
 import static com.flink.platform.common.constants.Constant.EMPTY;
 
@@ -25,16 +31,6 @@ public class OSUtil {
         return System.getProperty("os.name");
     }
 
-    /** Get local host ip. */
-    public static String getHostIp() {
-        try {
-            return InetAddress.getLocalHost().getHostAddress();
-        } catch (Exception e) {
-            log.error("Get ip address failed", e);
-            return EMPTY;
-        }
-    }
-
     /** Get hostname. */
     public static String getHostname() {
         try {
@@ -43,5 +39,43 @@ public class OSUtil {
             log.error("Get hostname failed", e);
             return EMPTY;
         }
+    }
+
+    /** Get host ip. */
+    public static String getFirstNoLoopbackIP4Address() {
+        try {
+            List<String> noLoopbackAddrs = new ArrayList<>();
+            List<String> siteLocalAddrs = new ArrayList<>();
+            for (InetAddress address : getAllHostAddress()) {
+                if (!address.isLoopbackAddress() && address instanceof Inet4Address) {
+                    if (address.isSiteLocalAddress()) {
+                        siteLocalAddrs.add(address.getHostAddress());
+                    } else {
+                        noLoopbackAddrs.add(address.getHostAddress());
+                    }
+                }
+            }
+
+            return noLoopbackAddrs.size() > 0
+                    ? noLoopbackAddrs.get(0)
+                    : (siteLocalAddrs.size() > 0 ? siteLocalAddrs.get(0) : null);
+        } catch (Exception e) {
+            throw new RuntimeException("Get ip address failed", e);
+        }
+    }
+
+    private static List<InetAddress> getAllHostAddress() throws SocketException {
+        Enumeration<NetworkInterface> networks = NetworkInterface.getNetworkInterfaces();
+        List<InetAddress> addresses = new ArrayList<>();
+        while (networks.hasMoreElements()) {
+            NetworkInterface networkInterface = networks.nextElement();
+            Enumeration<InetAddress> inetAddresses = networkInterface.getInetAddresses();
+            while (inetAddresses.hasMoreElements()) {
+                InetAddress inetAddress = inetAddresses.nextElement();
+                addresses.add(inetAddress);
+            }
+        }
+
+        return addresses;
     }
 }
