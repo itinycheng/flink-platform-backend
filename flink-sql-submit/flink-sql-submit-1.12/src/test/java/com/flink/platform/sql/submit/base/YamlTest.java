4260f982988c5af5f78c078ff7964043a09ba280
chenglong.wang <tiny.wcl@gmail.com>
2022-12-28 14:46:39 +0800

    Add maven wrapper and GitHub CI

--- flink-sql-submit/flink-sql-submit-1.12/src/test/java/com/flink/platform/sql/submit/base/YamlTest.java
+++ flink-sql-submit/flink-sql-submit-1.12/src/test/java/com/flink/platform/sql/submit/base/YamlTest.java
@@ -6,6 +6,8 @@ import org.junit.Test;
 import java.util.HashMap;
 import java.util.Map;
 
+import static org.junit.Assert.assertEquals;
+
 /** Unit test for simple App. */
 public class YamlTest {
 
@@ -13,9 +15,9 @@ public class YamlTest {
     public void mergeConfig() {
         Map<String, String> defaultConfig = ConfigLoader.loadDefault(ExecutionMode.BATCH);
         Map<String, String> configMap = new HashMap<>();
-        configMap.put("a", "1");
         configMap.put("parallelism.default", "2");
         defaultConfig.putAll(configMap);
         defaultConfig.forEach((s, s2) -> System.out.println(s + ", " + s2));
+        assertEquals(defaultConfig.get("parallelism.default"), "2");
     }
 }
