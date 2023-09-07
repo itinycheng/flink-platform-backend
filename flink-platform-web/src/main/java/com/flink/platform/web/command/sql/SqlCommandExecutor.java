c791835f609f5e64f1ccc231ba2ffee8c5df9a55
chenglong.wang <tiny.wcl@gmail.com>
2023-09-07 17:23:47 +0800

    feat: Cancel sql job

--- flink-platform-web/src/main/java/com/flink/platform/web/command/sql/SqlCommandExecutor.java
+++ flink-platform-web/src/main/java/com/flink/platform/web/command/sql/SqlCommandExecutor.java
@@ -1,13 +1,10 @@
 package com.flink.platform.web.command.sql;
 
-import com.flink.platform.common.enums.DbType;
 import com.flink.platform.common.enums.JobType;
-import com.flink.platform.common.util.ExceptionUtil;
-import com.flink.platform.common.util.JsonUtil;
 import com.flink.platform.dao.entity.Datasource;
 import com.flink.platform.dao.entity.result.JobCallback;
 import com.flink.platform.dao.service.DatasourceService;
-import com.flink.platform.dao.service.JobRunInfoService;
+import com.flink.platform.web.command.AbstractTask;
 import com.flink.platform.web.command.CommandExecutor;
 import com.flink.platform.web.command.JobCommand;
 import lombok.extern.slf4j.Slf4j;
@@ -16,23 +13,9 @@ import org.springframework.stereotype.Component;
 
 import javax.annotation.Nonnull;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import static com.flink.platform.common.constants.Constant.LINE_SEPARATOR;
-import static com.flink.platform.common.enums.ExecutionStatus.FAILURE;
-import static com.flink.platform.common.enums.ExecutionStatus.SUCCESS;
 import static com.flink.platform.common.enums.JobType.CLICKHOUSE_SQL;
 import static com.flink.platform.common.enums.JobType.HIVE_SQL;
 import static com.flink.platform.common.enums.JobType.MYSQL_SQL;
-import static com.flink.platform.web.util.JdbcUtil.createConnection;
-import static com.flink.platform.web.util.JdbcUtil.toJavaObject;
 
 /** SQL command executor. */
 @Slf4j
@@ -40,8 +23,6 @@ import static com.flink.platform.web.util.JdbcUtil.toJavaObject;
 public class SqlCommandExecutor implements CommandExecutor {
 
     @Autowired private DatasourceService datasourceService;
-
-    @Autowired private JobRunInfoService jobRunInfoService;
 
     @Override
     public boolean isSupported(JobType jobType) {
@@ -53,65 +34,18 @@ public class SqlCommandExecutor implements CommandExecutor {
     public JobCallback execCommand(@Nonnull JobCommand command) throws Exception {
         SqlCommand sqlCommand = (SqlCommand) command;
         Datasource datasource = datasourceService.getById(sqlCommand.getDsId());
+        SqlTask task = new SqlTask(sqlCommand.getJobRunId(), sqlCommand.getSqls(), datasource);
+        sqlCommand.setTask(task);
+        task.run();
 
-        Exception exception = null;
-        List<Map<String, Object>> dataList = new ArrayList<>();
-        try (Connection connection =
-                        createConnection(datasource.getType(), datasource.getParams());
-                Statement stmt = connection.createStatement()) {
-            String executingSql = null;
-            try {
-                for (int j = 0; j < sqlCommand.getSqls().size(); j++) {
-                    executingSql = sqlCommand.getSqls().get(j);
+        return task.buildResult();
+    }
 
-                    // execute sq.
-                    if (!stmt.execute(executingSql)) {
-                        Map<String, Object> itemMap = new HashMap<>(1);
-                        itemMap.put("_no_result_" + j, String.format("executed: %s", executingSql));
-                        dataList.add(itemMap);
-                        continue;
-                    }
-
-                    try (ResultSet resultSet = stmt.getResultSet()) {
-                        // metadata.
-                        ResultSetMetaData metaData = resultSet.getMetaData();
-                        int num = metaData.getColumnCount();
-                        String[] columnNames = new String[num];
-                        for (int i = 1; i <= num; i++) {
-                            columnNames[i - 1] = metaData.getColumnName(i);
-                        }
-
-                        // data list.
-                        DbType dbType = datasource.getType();
-                        int count = 0;
-                        while (resultSet.next() && count++ < 2000) {
-                            Map<String, Object> itemMap = new HashMap<>(num);
-                            for (int i = 1; i <= num; i++) {
-                                itemMap.put(
-                                        columnNames[i - 1],
-                                        toJavaObject(resultSet.getObject(i), dbType));
-                            }
-                            dataList.add(itemMap);
-                        }
-                    }
-
-                    log.info("Execute sql successfully: {}", executingSql);
-                }
-            } catch (Exception e) {
-                exception = e;
-                log.error("Execute sql: {} failed.", executingSql, e);
-            }
+    @Override
+    public void killCommand(@Nonnull JobCommand command) {
+        AbstractTask task = command.getTask();
+        if (task != null) {
+            task.cancel();
         }
-
-        String exceptionMsg = null;
-        if (exception != null) {
-            exceptionMsg = ExceptionUtil.stackTrace(exception);
-        }
-
-        boolean isSucceed = exceptionMsg == null;
-        String dataString = JsonUtil.toJsonString(dataList);
-        return new JobCallback(
-                isSucceed ? dataString : String.join(LINE_SEPARATOR, dataString, exceptionMsg),
-                isSucceed ? SUCCESS : FAILURE);
     }
 }
---
+++ flink-platform-web/src/main/java/com/flink/platform/web/command/sql/SqlTask.java
@@ -0,0 +1,153 @@
+package com.flink.platform.web.command.sql;
+
+import com.flink.platform.common.enums.DbType;
+import com.flink.platform.common.util.ExceptionUtil;
+import com.flink.platform.common.util.JsonUtil;
+import com.flink.platform.dao.entity.Datasource;
+import com.flink.platform.dao.entity.result.JobCallback;
+import com.flink.platform.web.command.AbstractTask;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.annotation.Nonnull;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static com.flink.platform.common.constants.Constant.LINE_SEPARATOR;
+import static com.flink.platform.common.enums.ExecutionStatus.FAILURE;
+import static com.flink.platform.common.enums.ExecutionStatus.SUCCESS;
+import static com.flink.platform.common.util.Preconditions.checkNotNull;
+import static com.flink.platform.web.util.JdbcUtil.createConnection;
+import static com.flink.platform.web.util.JdbcUtil.toJavaObject;
+
+/** SQL Task. */
+@Slf4j
+@Getter
+@Setter
+public class SqlTask extends AbstractTask {
+
+    private final Datasource datasource;
+
+    private final List<String> sqlList;
+
+    private Connection connection;
+
+    private Statement statement;
+
+    private boolean isSucceed;
+
+    private String exceptionStack;
+
+    private String sqlResult;
+
+    public SqlTask(long jobRunId, @Nonnull List<String> sqlList, @Nonnull Datasource datasource) {
+        super(jobRunId);
+        this.sqlList = checkNotNull(sqlList);
+        this.datasource = checkNotNull(datasource);
+    }
+
+    @Override
+    public void run() throws Exception {
+        Exception exception = null;
+        List<Map<String, Object>> dataList = new ArrayList<>();
+        try (Connection connection =
+                        createConnection(datasource.getType(), datasource.getParams());
+                Statement stmt = connection.createStatement()) {
+            this.connection = connection;
+            this.statement = stmt;
+
+            String executingSql = null;
+            try {
+                for (int j = 0; j < sqlList.size(); j++) {
+                    executingSql = sqlList.get(j);
+
+                    // execute sql.
+                    if (!stmt.execute(executingSql)) {
+                        Map<String, Object> itemMap = new HashMap<>(1);
+                        itemMap.put("_no_result_" + j, String.format("executed: %s", executingSql));
+                        dataList.add(itemMap);
+                        continue;
+                    }
+
+                    try (ResultSet resultSet = stmt.getResultSet()) {
+                        // metadata.
+                        ResultSetMetaData metaData = resultSet.getMetaData();
+                        int num = metaData.getColumnCount();
+                        String[] columnNames = new String[num];
+                        for (int i = 1; i <= num; i++) {
+                            columnNames[i - 1] = metaData.getColumnName(i);
+                        }
+
+                        // data list.
+                        DbType dbType = datasource.getType();
+                        int count = 0;
+                        while (resultSet.next() && count++ < 2000) {
+                            Map<String, Object> itemMap = new HashMap<>(num);
+                            for (int i = 1; i <= num; i++) {
+                                itemMap.put(
+                                        columnNames[i - 1],
+                                        toJavaObject(resultSet.getObject(i), dbType));
+                            }
+                            dataList.add(itemMap);
+                        }
+                    }
+
+                    log.info("Execute sql successfully: {}", executingSql);
+                }
+            } catch (Exception e) {
+                exception = e;
+                log.error("Execute sql: {} failed.", executingSql, e);
+            }
+        }
+
+        String exceptionMsg = null;
+        if (exception != null) {
+            exceptionMsg = ExceptionUtil.stackTrace(exception);
+        }
+
+        this.isSucceed = exceptionMsg == null;
+        this.exceptionStack = exceptionMsg;
+        this.sqlResult = JsonUtil.toJsonString(dataList);
+    }
+
+    @Override
+    public void cancel() {
+        if (statement != null) {
+            try {
+                statement.cancel();
+            } catch (Exception e) {
+                log.error("Cancel jdbc statement failed", e);
+            }
+
+            try {
+                if (!statement.isClosed()) {
+                    statement.close();
+                }
+            } catch (Exception e) {
+                log.error("Close jdbc statement failed", e);
+            }
+        }
+
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (Exception e) {
+                log.error("Close jdbc connection failed", e);
+            }
+        }
+    }
+
+    public JobCallback buildResult() {
+        return new JobCallback(
+                isSucceed ? sqlResult : String.join(LINE_SEPARATOR, sqlResult, exceptionStack),
+                isSucceed ? SUCCESS : FAILURE);
+    }
+}
