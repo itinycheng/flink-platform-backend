e0b9c34b6e98d3f57b9e3367d6b055a884f3ef8e
chenglong.wang <tiny.wcl@gmail.com>
2023-12-26 21:44:11 +0800

    improve: Get or init grpc client by ip

--- flink-platform-web/src/main/java/com/flink/platform/web/grpc/JobProcessGrpcClient.java
+++ flink-platform-web/src/main/java/com/flink/platform/web/grpc/JobProcessGrpcClient.java
@@ -1,7 +1,9 @@
 package com.flink.platform.web.grpc;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.flink.platform.common.constants.Constant;
 import com.flink.platform.dao.entity.Worker;
+import com.flink.platform.dao.service.WorkerService;
 import com.flink.platform.grpc.JobGrpcServiceGrpc;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
@@ -9,6 +11,7 @@ import jakarta.annotation.PreDestroy;
 import lombok.extern.slf4j.Slf4j;
 import net.devh.boot.grpc.client.inject.GrpcClient;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
@@ -22,6 +25,9 @@ import static com.flink.platform.grpc.JobGrpcServiceGrpc.JobGrpcServiceBlockingS
 @Service
 public class JobProcessGrpcClient {
 
+    @Autowired
+    private WorkerService workerService;
+
     @GrpcClient("local-grpc-server")
     private JobGrpcServiceBlockingStub localGrpcStub;
 
@@ -29,41 +35,39 @@ public class JobProcessGrpcClient {
 
     private final Map<String, ManagedChannel> channelMap = new ConcurrentHashMap<>();
 
-    private final Object lock = new Object();
-
-    public JobGrpcServiceBlockingStub grpcClient(Worker worker) {
-        if (worker == null
-                || StringUtils.isEmpty(worker.getIp())
-                || Constant.LOCALHOST.equals(worker.getIp())
-                || Constant.HOST_IP.equals(worker.getIp())) {
+    public JobGrpcServiceBlockingStub grpcClient(String ip) {
+        if (StringUtils.isEmpty(ip) || Constant.LOCALHOST.equals(ip) || Constant.HOST_IP.equals(ip)) {
             return localGrpcStub;
         }
 
-        String key = stubKey(worker.getIp(), worker.getGrpcPort());
+        String key = stubKey(ip);
         JobGrpcServiceBlockingStub stub = grpcStubMap.get(key);
         if (stub != null) {
             return stub;
         }
 
-        synchronized (lock) {
-            stub = grpcStubMap.get(key);
-            if (stub != null) {
-                return stub;
-            }
+        Worker worker = workerService.getOne(new QueryWrapper<Worker>().lambda().eq(Worker::getIp, ip));
+        return grpcClient(key, worker);
+    }
 
-            ManagedChannel channel = channelMap.computeIfAbsent(
-                    key, s -> ManagedChannelBuilder.forAddress(worker.getIp(), worker.getGrpcPort())
-                            .keepAliveWithoutCalls(true)
-                            .usePlaintext()
-                            .build());
-            stub = JobGrpcServiceGrpc.newBlockingStub(channel);
-            grpcStubMap.put(key, stub);
+    public synchronized JobGrpcServiceBlockingStub grpcClient(String key, Worker worker) {
+        JobGrpcServiceBlockingStub stub = grpcStubMap.get(key);
+        if (stub != null) {
+            return stub;
         }
+
+        ManagedChannel channel = channelMap.computeIfAbsent(
+                key, s -> ManagedChannelBuilder.forAddress(worker.getIp(), worker.getGrpcPort())
+                        .keepAliveWithoutCalls(true)
+                        .usePlaintext()
+                        .build());
+        stub = JobGrpcServiceGrpc.newBlockingStub(channel);
+        grpcStubMap.put(key, stub);
         return stub;
     }
 
-    private String stubKey(String ip, int port) {
-        return String.join(":", ip, String.valueOf(port));
+    private String stubKey(String ip) {
+        return String.valueOf(ip).trim();
     }
 
     @PreDestroy
