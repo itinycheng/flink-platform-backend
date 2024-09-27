dc91461d672beb31241f8029dd26d4a48c046abf
chenglong.wang <tiny.wcl@gmail.com>
2024-09-27 18:55:02 +0800

    feat: Add module `flink-platform-plugin`

---
+++ flink-platform-plugin/flink-platform-plugin-apollo/src/test/java/com/flink/platform/apollo/AppTest.java
@@ -0,0 +1,11 @@
+package com.flink.platform.apollo;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
