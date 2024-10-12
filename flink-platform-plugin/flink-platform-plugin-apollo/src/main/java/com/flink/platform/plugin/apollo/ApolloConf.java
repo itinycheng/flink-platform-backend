4dbadebbb02d068e901c9263ed2a54e97c652779
chenglong.wang <tiny.wcl@gmail.com>
2024-10-12 14:58:10 +0800

    feat: Add jasypt to apollo plugin

--- flink-platform-plugin/flink-platform-plugin-apollo/src/main/java/com/flink/platform/plugin/apollo/ApolloConf.java
+++ flink-platform-plugin/flink-platform-plugin-apollo/src/main/java/com/flink/platform/plugin/apollo/ApolloConf.java
@@ -4,6 +4,10 @@ import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 /**
  * apollo configuration.
  */
@@ -11,8 +15,28 @@ import org.springframework.context.annotation.Configuration;
 @EnableApolloConfig
 public class ApolloConf {
 
+    private static final String CONFIG = "META-INF/app.properties";
+
+    private static final String SALT = "apollo.salt";
+
+    private final String secretKey;
+
+    public ApolloConf() {
+        try (InputStream input = Thread.currentThread().getContextClassLoader().getResourceAsStream(CONFIG)) {
+            if (input == null) {
+                throw new IOException("No apollo config file found, path: " + CONFIG);
+            }
+
+            Properties properties = new Properties();
+            properties.load(input);
+            this.secretKey = properties.getProperty(SALT);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to load " + CONFIG, e);
+        }
+    }
+
     @Bean
     public DatasourceNamespaceBean datasourceNamespaceBean() {
-        return new DatasourceNamespaceBean();
+        return new DatasourceNamespaceBean(secretKey);
     }
 }
