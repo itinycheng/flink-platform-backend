beb1e9d20f48e638f6597c590f41b8e0da1f2957
suxinshuo <suxinshuo@itiger.com>
2025-04-29 10:06:54 +0800

    *) 移除 spark sql 中的参数设置

---
+++ flink-platform-web/src/test/java/com/flink/platform/web/command/sql/HiveSqlTaskTest.java
@@ -0,0 +1,36 @@
+package com.flink.platform.web.command.sql;
+
+import com.flink.platform.dao.entity.Datasource;
+import com.google.common.collect.Lists;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
+public class HiveSqlTaskTest {
+
+    @Test
+    public void getSparkConfTest() {
+        List<String> sqlList = Lists.newArrayList("set spark.dynamicAllocation.maxExecutors=10", "select 1");
+        HiveSqlTask hiveSqlTask = new HiveSqlTask(1L, sqlList, new Datasource());
+        Map<String, String> sparkConf = hiveSqlTask.getSparkConf(sqlList);
+        log.info("sparkConf: {}", sparkConf);
+        hiveSqlTask.beforeExecSql();
+        List<String> execSqlList = hiveSqlTask.getSqlList();
+        log.info("execSqlList: {}", execSqlList);
+    }
+
+    @Test
+    public void getSparkConfTest2() {
+        List<String> sqlList = Lists.newArrayList(
+                "create xxx;", "SET spark.sql.shuffle.partitions = 1;", "create taera;", "insert overwrite select 1;");
+        HiveSqlTask hiveSqlTask = new HiveSqlTask(1L, sqlList, new Datasource());
+        Map<String, String> sparkConf = hiveSqlTask.getSparkConf(sqlList);
+        log.info("sparkConf: {}", sparkConf);
+        hiveSqlTask.beforeExecSql();
+        List<String> execSqlList = hiveSqlTask.getSqlList();
+        log.info("execSqlList: {}", execSqlList);
+    }
+}
