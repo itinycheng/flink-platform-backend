9613ac9180d7ae8b6cc2e16a10684e02c7546e19
chenglong.wang <tiny.wcl@gmail.com>
2025-10-20 10:53:33 +0800

    fix: Update application tag format in YarnHelper and simplify code
    To maintain backward compatibility, The tag format shouldn't be changed before saving to db

--- flink-platform-web/src/main/java/com/flink/platform/web/service/ProcessJobService.java
+++ flink-platform-web/src/main/java/com/flink/platform/web/service/ProcessJobService.java
@@ -1,26 +1,19 @@
 package com.flink.platform.web.service;
 
-import com.flink.platform.common.enums.ExecutionStatus;
-import com.flink.platform.common.enums.JobType;
 import com.flink.platform.common.exception.UnrecoverableException;
 import com.flink.platform.common.util.JsonUtil;
 import com.flink.platform.dao.entity.JobRunInfo;
-import com.flink.platform.dao.entity.result.JobCallback;
 import com.flink.platform.dao.entity.task.BaseJob;
 import com.flink.platform.dao.entity.task.FlinkJob;
 import com.flink.platform.dao.service.JobRunInfoService;
 import com.flink.platform.web.command.CommandBuilder;
 import com.flink.platform.web.command.CommandExecutor;
-import com.flink.platform.web.command.JobCommand;
-import com.flink.platform.web.command.flink.FlinkCommand;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
@@ -42,85 +35,67 @@ public class ProcessJobService {
     private final List<CommandExecutor> jobCommandExecutors;
 
     public void processJob(final long jobRunId) throws Exception {
-        JobCommand jobCommand = null;
-        JobRunInfo jobRunInfo = null;
-
-        try {
-            // step 1: get job info
-            jobRunInfo = jobRunInfoService.getById(jobRunId);
-            if (jobRunInfo == null) {
-                throw new UnrecoverableException(String.format("The job run: %s is no longer exists.", jobRunId));
-            }
-
-            // step 2: Update jobRun and prepare environment before execution.
-            Map<String, Object> varMap = jobRunExtraService.parseVariables(jobRunInfo);
-            String replacedContent = replaceVariables(jobRunInfo.getSubject(), varMap);
-            BaseJob replacedConfig = replaceVariables(jobRunInfo.getConfig(), varMap);
-            if (replacedConfig == null) {
-                replacedConfig = jobRunInfo.getConfig();
-            }
-
-            // create submit time.
-            LocalDateTime submitTime = LocalDateTime.now();
-
-            jobRunInfo.setVariables(varMap);
-            jobRunInfo.setSubject(replacedContent);
-            jobRunInfo.setConfig(replacedConfig);
-            jobRunInfo.setSubmitTime(submitTime);
-
-            JobRunInfo newJobRun = new JobRunInfo();
-            newJobRun.setId(jobRunInfo.getId());
-            newJobRun.setVariables(varMap);
-            newJobRun.setSubject(replacedContent);
-            newJobRun.setConfig(replacedConfig);
-            newJobRun.setHost(HOST_IP);
-            newJobRun.setSubmitTime(submitTime);
-            jobRunInfoService.updateById(newJobRun);
-
-            JobType jobType = jobRunInfo.getType();
-            String version = jobRunInfo.getVersion();
-
-            // step 3: build job command, create a SqlContext if needed
-            jobCommand = jobCommandBuilders.stream()
-                    .filter(builder -> builder.isSupported(jobType, version))
-                    .findFirst()
-                    .orElseThrow(() -> new UnrecoverableException("No available job command builder"))
-                    .buildCommand(jobRunInfo.getFlowRunId(), jobRunInfo);
-
-            // step 4: submit job
-            final var command = jobCommand;
-            JobCallback callback = jobCommandExecutors.stream()
-                    .filter(executor -> executor.isSupported(jobType))
-                    .findFirst()
-                    .orElseThrow(() -> new UnrecoverableException("No available job command executor"))
-                    .exec(command);
-
-            // step 5: write job run info to db
-            ExecutionStatus executionStatus = callback.getStatus();
-            newJobRun = new JobRunInfo();
-            newJobRun.setId(jobRunInfo.getId());
-            newJobRun.setStatus(executionStatus);
-            newJobRun.setBackInfo(callback);
-            if (executionStatus.isTerminalState()) {
-                newJobRun.setEndTime(LocalDateTime.now());
-            }
-            jobRunInfoService.updateById(newJobRun);
-
-            // step 6: print job command info
-            log.info("Job run: {} submitted, time: {}", jobRunId, System.currentTimeMillis());
-
-        } finally {
-            if (jobRunInfo != null && jobRunInfo.getType() == JobType.FLINK_SQL && jobCommand != null) {
-                try {
-                    FlinkCommand flinkCommand = (FlinkCommand) jobCommand;
-                    if (flinkCommand.getMainArgs() != null) {
-                        Files.deleteIfExists(Paths.get(flinkCommand.getMainArgs()));
-                    }
-                } catch (Exception e) {
-                    log.warn("Delete sql context file failed", e);
-                }
-            }
+        // step 1: get job info
+        var jobRunInfo = jobRunInfoService.getById(jobRunId);
+        if (jobRunInfo == null) {
+            throw new UnrecoverableException(String.format("The job run: %s is no longer exists.", jobRunId));
         }
+
+        // step 2: Update jobRun and prepare environment before execution.
+        var varMap = jobRunExtraService.parseVariables(jobRunInfo);
+        var replacedContent = replaceVariables(jobRunInfo.getSubject(), varMap);
+        var replacedConfig = replaceVariables(jobRunInfo.getConfig(), varMap);
+        if (replacedConfig == null) {
+            replacedConfig = jobRunInfo.getConfig();
+        }
+
+        // create submit time.
+        var submitTime = LocalDateTime.now();
+
+        jobRunInfo.setVariables(varMap);
+        jobRunInfo.setSubject(replacedContent);
+        jobRunInfo.setConfig(replacedConfig);
+        jobRunInfo.setSubmitTime(submitTime);
+
+        var newJobRun = new JobRunInfo();
+        newJobRun.setId(jobRunInfo.getId());
+        newJobRun.setVariables(varMap);
+        newJobRun.setSubject(replacedContent);
+        newJobRun.setConfig(replacedConfig);
+        newJobRun.setHost(HOST_IP);
+        newJobRun.setSubmitTime(submitTime);
+        jobRunInfoService.updateById(newJobRun);
+
+        var jobType = jobRunInfo.getType();
+        var version = jobRunInfo.getVersion();
+
+        // step 3: build job command, create a SqlContext if needed
+        var command = jobCommandBuilders.stream()
+                .filter(builder -> builder.isSupported(jobType, version))
+                .findFirst()
+                .orElseThrow(() -> new UnrecoverableException("No available job command builder"))
+                .buildCommand(jobRunInfo.getFlowRunId(), jobRunInfo);
+
+        // step 4: submit job
+        var callback = jobCommandExecutors.stream()
+                .filter(executor -> executor.isSupported(jobType))
+                .findFirst()
+                .orElseThrow(() -> new UnrecoverableException("No available job command executor"))
+                .exec(command);
+
+        // step 5: write job run info to db
+        var executionStatus = callback.getStatus();
+        newJobRun = new JobRunInfo();
+        newJobRun.setId(jobRunInfo.getId());
+        newJobRun.setStatus(executionStatus);
+        newJobRun.setBackInfo(callback);
+        if (executionStatus.isTerminalState()) {
+            newJobRun.setEndTime(LocalDateTime.now());
+        }
+        jobRunInfoService.updateById(newJobRun);
+
+        // step 6: print job command info
+        log.info("Job run: {} submitted, time: {}", jobRunId, System.currentTimeMillis());
     }
 
     private BaseJob replaceVariables(BaseJob baseJob, Map<String, Object> varValueMap) {
@@ -129,8 +104,8 @@ public class ProcessJobService {
             return baseJob;
         }
 
-        String jsonString = JsonUtil.toJsonString(baseJob);
-        String replaced = replaceVariables(jsonString, varValueMap);
+        var jsonString = JsonUtil.toJsonString(baseJob);
+        var replaced = replaceVariables(jsonString, varValueMap);
         return JsonUtil.toBean(replaced, BaseJob.class);
     }
 
