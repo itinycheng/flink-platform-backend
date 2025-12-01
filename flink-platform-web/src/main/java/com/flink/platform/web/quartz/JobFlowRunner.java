931c55fd826a693cd951c0a880f29a5d5cb54e6c
chenglong.wang <tiny.wcl@gmail.com>
2025-12-01 10:13:03 +0800

    fix: Handle exceptions during subflow submission

--- flink-platform-web/src/main/java/com/flink/platform/web/quartz/JobFlowRunner.java
+++ flink-platform-web/src/main/java/com/flink/platform/web/quartz/JobFlowRunner.java
@@ -54,6 +54,7 @@ public class JobFlowRunner implements Job {
 
     private final WorkerConfig workerConfig = SpringContext.getBean(WorkerConfig.class);
 
+    // TODO: handle exceptions ?
     @Override
     public void execute(JobExecutionContext context) {
         var detail = context.getJobDetail();
