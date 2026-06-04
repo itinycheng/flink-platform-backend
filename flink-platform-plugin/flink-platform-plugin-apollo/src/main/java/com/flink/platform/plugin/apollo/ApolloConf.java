d4a0346fab27248f067deb452ecc13e605e7beeb
chenglong.wang <tiny.wcl@gmail.com>
2026-06-04 09:55:23 +0800

    refactor(apollo): Defer apollo client init to first getConfig call

--- flink-platform-plugin/flink-platform-plugin-apollo/src/main/java/com/flink/platform/plugin/apollo/ApolloConf.java
+++ flink-platform-plugin/flink-platform-plugin-apollo/src/main/java/com/flink/platform/plugin/apollo/ApolloConf.java
@@ -1,9 +1,12 @@
 package com.flink.platform.plugin.apollo;
 
 import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 
 /**
  * Apollo configuration. Bootstrap properties are imported from {@code apollo-<profile>.properties}
@@ -12,8 +15,9 @@ import org.springframework.context.annotation.Configuration;
  * apollo.cluster, apollo.cache-dir, env) into system properties before {@code ConfigService}
  * initializes.
  */
+@Slf4j
 @Configuration
-@EnableApolloConfig
+@EnableApolloConfig({}) // empty: no namespace eagerly loaded.
 public class ApolloConf {
 
     private final String secretKey;
@@ -23,7 +27,9 @@ public class ApolloConf {
     }
 
     @Bean
+    @Lazy
     public DatasourceNamespaceBean datasourceNamespaceBean() {
+        log.info("Initializing datasource namespace bean (lazy), salt loaded: {}", StringUtils.isNotBlank(secretKey));
         return new DatasourceNamespaceBean(secretKey);
     }
 }
